b261f4086e4317455e6b564ef76a53793f52fa9a
arshiya923 <gh.arshiya92@gmail.com>
2019-05-03 13:03:51 +0430

    Add files via upload

---
+++ Fasl1 - Tamrin04/src/fasl1/tamrin4/Fasl1Tamrin4.java
@@ -0,0 +1,11 @@
+package fasl1.tamrin4;
+public class Fasl1Tamrin4
+{
+public static void main(String[] args)
+    {
+        System.out.print("name\t\tlast name\n");
+        System.out.print("mohammad\tsalimi\n");
+        System.out.print("c:\\myfolder\\cplusplus\n");
+    }
+    
+}
