651696550ea5c848a51c91d9f8216ac826e5613d
arshiya923 <gh.arshiya92@gmail.com>
2019-05-03 13:09:30 +0430

    Add files via upload

---
+++ Fasl1 - Tamrin06/src/fasl1/tamrin6/Fasl1Tamrin6.java
@@ -0,0 +1,14 @@
+package fasl1.tamrin6;
+import java.util.Scanner;
+
+
+public class Fasl1Tamrin6
+{
+public static void main(String[] args) 
+    {
+      Scanner obj=new Scanner(System.in);  
+        
+        
+    }
+    
+}
