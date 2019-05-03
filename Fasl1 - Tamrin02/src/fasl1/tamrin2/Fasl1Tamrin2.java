71264c872264e8d4e31f3688bdbd4210098ed959
arshiya923 <gh.arshiya92@gmail.com>
2019-05-03 12:42:04 +0430

    Add files via upload

---
+++ Fasl1 - Tamrin02/src/fasl1/tamrin2/Fasl1Tamrin2.java
@@ -0,0 +1,10 @@
+package fasl1.tamrin2;
+public class Fasl1Tamrin2
+{
+public static void main(String[] args)
+    {
+        System.out.println("Amir");
+        System.out.println("Daneshvar");
+    }
+    
+}
